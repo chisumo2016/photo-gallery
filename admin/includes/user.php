3e189a60a2784b064f1a2d35917e67b249a53d62
chisumo <bchisumo74@yahoo.com>
2021-08-02 17:46:49 +0100

    Created the User Class successfully

---
+++ admin/includes/user.php
@@ -0,0 +1,12 @@
+<?php
+
+Class User {
+
+    public function  find_all_users()
+    {
+        global $database;
+        $result_set = $database->query("SELECT * FROM users");
+        return $result_set;
+    }
+
+}
