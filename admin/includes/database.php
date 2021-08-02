880e046af6fa08c6a43b87582bf2c12b46071077
chisumo <bchisumo74@yahoo.com>
2021-08-02 14:42:11 +0100

    Updating the private connection to public

--- admin/includes/database.php
+++ admin/includes/database.php
@@ -4,7 +4,12 @@ require_once ("new_config.php");
 
 class Database {
 
-      private $connection;
+      public $connection;
+
+      public function __construct()
+      {
+          $this->open_db_connection();
+      }
 
 
     public function open_db_connection()
