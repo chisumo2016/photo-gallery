fc691ed7b6f2c3fed9b5ee10fcfa47f5a4ff7666
chisumo <bchisumo74@yahoo.com>
2021-08-02 17:56:19 +0100

    Testing our find_all method() for user class

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -9,10 +9,12 @@
             </h1>
 
             <?php
-                   $sql = "SELECT * FROM  users WHERE id= 1";
-                   $result = $database->query($sql);
-                   $user_found = mysqli_fetch_array($result);
-                   echo $user_found['last_name'];
+                   $user = new User();
+                   $result_set = $user->find_all_users();
+
+                   while($row = mysqli_fetch_array($result_set)){
+                       echo $row['username'] . "<br>";
+                   }
             ?>
             <ol class="breadcrumb">
                 <li>
