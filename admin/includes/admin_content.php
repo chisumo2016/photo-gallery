7f23d66b967e47d7279c64153d5abd4eed5d7e53
chisumo <bchisumo74@yahoo.com>
2021-08-02 17:34:26 +0100

    Improvement on the Connection to behave a  OOP

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -9,9 +9,10 @@
             </h1>
 
             <?php
-                if ($database->connection){
-                    echo "rue";
-                }
+                   $sql = "SELECT * FROM  users WHERE id= 1";
+                   $result = $database->query($sql);
+                   $user_found = mysqli_fetch_array($result);
+                   echo $user_found['last_name'];
             ?>
             <ol class="breadcrumb">
                 <li>
