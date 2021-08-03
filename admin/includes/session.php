7b48d932fa631f33df71471f2fa50b096bf490db
chisumo <bchisumo74@yahoo.com>
2021-08-03 13:39:44 +0100

    Implemented the Login Method in Session class

--- admin/includes/session.php
+++ admin/includes/session.php
@@ -2,23 +2,37 @@
 
   class Session
   {
-      private  $signed_in;
+      private  $signed_in = false;
       public   $user_id;
 
       function  __construct(){
           session_start();
+          $this->check_the_login();
       }
 
       private  function check_the_login()
       {
           if (isset($_session['user_id'])){
-              $this->user_id = $_session['user_id'];
+              $this->user_id = $_SESSION['user_id'];
               $this->signed_in = true;
               //we dont find
           }else{
               unset($this->user_id);
               $this->signed_in = false;
           }
+      }
+
+      public  function  is_signed_in() //getter
+      {
+          return $this->signed_in;
+      }
+
+      public  function  login($user) //$user come from database
+      {
+         if ($user){
+             $this->user_id = $_SESSION['user_id'] = $user->id;
+             $this->signed_in =true;
+         }
       }
   }
 
