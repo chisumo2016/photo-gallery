2ea69bae66e4a72c5bd2a37a185bace6746b0798
chisumo <bchisumo74@yahoo.com>
2021-08-03 12:16:22 +0100

    Implemented the checking login method in the session

--- admin/includes/session.php
+++ admin/includes/session.php
@@ -3,11 +3,24 @@
   class Session
   {
       private  $signed_in;
-      public $user_id;
+      public   $user_id;
 
       function  __construct(){
           session_start();
       }
+
+      private  function check_the_login()
+      {
+          if (isset($_session['user_id'])){
+              $this->user_id = $_session['user_id'];
+              $this->signed_in = true;
+              //we dont find
+          }else{
+              unset($this->user_id);
+              $this->signed_in = false;
+          }
+      }
   }
+
 
   $session = new Session();
