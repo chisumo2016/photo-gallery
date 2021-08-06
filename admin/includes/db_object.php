8982fb1c4c8da12afb3bcafc359aaaab80e77933
chisumo <bchisumo74@yahoo.com>
2021-08-06 08:46:37 +0100

    Implemented the parent class (inheritance) callaed db_object class

---
+++ admin/includes/db_object.php
@@ -0,0 +1,62 @@
+<?php
+
+class  db_object{
+
+
+    public static  function  find_all()
+    {
+        global $database;
+
+        return self::find_this_query("SELECT * FROM " .self::$db_table . " ");
+
+    }
+
+    public static  function find_by_id($user_id)
+    {
+        global $database;
+
+        $the_result_array = self::find_this_query("SELECT * FROM " .self::$db_table . " WHERE id = $user_id LIMIT 1");
+
+        return !empty($the_result_array) ? array_shift($the_result_array) : false;
+
+    }
+
+    public  static  function  find_this_query($sql)
+    {
+        global $database;
+        $result_set = $database->query($sql);
+
+        $the_object_array = [];
+
+        while($row = mysqli_fetch_array($result_set )){
+            $the_object_array[] = self::instantantion($row);
+        }
+        return $the_object_array ;
+    }
+
+
+
+    public  static function instantantion($the_record)
+    {
+        $the_object = new self;
+
+        foreach ($the_record as  $the_attribute => $value) {
+
+            if ($the_object->has_the_attribute($the_attribute)){
+                //assign the property a value
+                $the_object->$the_attribute = $value;
+            }
+        }
+        return $the_object;
+    }
+
+    private  function  has_the_attribute($the_attribute)
+    {
+        $object_properties = get_object_vars($this);
+        //if the key exist
+        return  array_key_exists($the_attribute, $object_properties);
+    }
+
+
+
+}
--- admin/includes/user_copy.php
+++ admin/includes/user_copy.php
@@ -20,7 +20,63 @@ Class User  extends  db_object {
 
 
 
+    public static  function  find_all()
+    {
+         global $database;
 
+        return self::find_this_query("SELECT * FROM " .self::$db_table . " ");
+
+        //return self::find_this_query("SELECT * FROM users");
+
+        /*$result_set = $database->query("SELECT * FROM users");
+        return $result_set;*/
+    }
+
+    public static  function find_by_id($user_id)
+    {
+        global $database;
+       // $result_set = $database->query("SELECT * FROM users WHERE id = $user_id LIMIT 1");
+
+        //$the_result_array = self::find_this_query("SELECT * FROM users WHERE id = $user_id LIMIT 1");
+        $the_result_array = self::find_this_query("SELECT * FROM " .self::$db_table . " WHERE id = $user_id LIMIT 1");
+
+        return !empty($the_result_array) ? array_shift($the_result_array) : false;
+
+    }
+
+    public  static  function  find_this_query($sql)
+    {
+        global $database;
+        $result_set = $database->query($sql);
+
+        $the_object_array = [];
+
+        while($row = mysqli_fetch_array($result_set )){
+            $the_object_array[] = self::instantantion($row);
+        }
+        return $the_object_array ;
+    }
+
+    public  static function instantantion($the_record)
+    {
+        $the_object = new self;
+
+        foreach ($the_record as  $the_attribute => $value) {
+
+            if ($the_object->has_the_attribute($the_attribute)){
+                //assign the property a value
+                $the_object->$the_attribute = $value;
+            }
+        }
+        return $the_object;
+    }
+
+    private  function  has_the_attribute($the_attribute)
+    {
+       $object_properties = get_object_vars($this);
+       //if the key exist
+       return  array_key_exists($the_attribute, $object_properties);
+    }
 
     public  static  function  verify_user($username,$password)
     {
