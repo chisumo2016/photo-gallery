f1513ee6d771cf72517b90c6423971a91fb391f4
chisumo <bchisumo74@yahoo.com>
2021-08-06 08:23:10 +0100

    Implemented the  rest of abstract method to the part of application in user class

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -20,22 +20,25 @@ Class User {
 
 
 
-    public static  function  find_all_users()
+    public static  function  find_all()
     {
          global $database;
 
-        return self::find_this_query("SELECT * FROM users");
+        return self::find_this_query("SELECT * FROM " .self::$db_table . " ");
+
+        //return self::find_this_query("SELECT * FROM users");
 
         /*$result_set = $database->query("SELECT * FROM users");
         return $result_set;*/
     }
 
-    public static  function find_user_by_id($user_id)
+    public static  function find_by_id($user_id)
     {
         global $database;
        // $result_set = $database->query("SELECT * FROM users WHERE id = $user_id LIMIT 1");
 
-        $the_result_array = self::find_this_query("SELECT * FROM users WHERE id = $user_id LIMIT 1");
+        //$the_result_array = self::find_this_query("SELECT * FROM users WHERE id = $user_id LIMIT 1");
+        $the_result_array = self::find_this_query("SELECT * FROM " .self::$db_table . " WHERE id = $user_id LIMIT 1");
 
         return !empty($the_result_array) ? array_shift($the_result_array) : false;
 
@@ -82,8 +85,7 @@ Class User {
         $username = $database->escape_string($username);
         $password = $database->escape_string($password);
 
-
-        $sql = "SELECT * FROM users WHERE ";
+        $sql = "SELECT * FROM " .self::$db_table . " WHERE ";
         $sql .= "username = '$username' AND ";
         $sql .= "password = '$password'";
 
