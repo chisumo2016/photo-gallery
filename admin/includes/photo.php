036e451b25583647d9e4c3b05aca7144944589f8
chisumo <bchisumo74@yahoo.com>
2021-08-08 13:24:11 +0100

    added some of properties in photo class

--- admin/includes/photo.php
+++ admin/includes/photo.php
@@ -3,8 +3,24 @@
 
 class  Photo extends  Db_object
 {
+    //static properties
+    protected static  $db_table ="photos";
+    protected static $db_table_fields = [
+        'id',
+        'title',
+        'description',
+        'filename',
+        'type',
+        'size'
+    ];
 
-
+    // properties
+    public $id;
+    public $title;
+    public $description;
+    public $filename;
+    public $type;
+    public $size;
 
 
 
