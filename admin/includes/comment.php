c66e570f5c7054b4c042bce84d5d60092f004607
chisumo <bchisumo74@yahoo.com>
2021-08-09 13:58:16 +0100

    Implemented the  Self Instantiation Comment Method

--- admin/includes/comment.php
+++ admin/includes/comment.php
@@ -5,17 +5,31 @@ Class Comment  extends  Db_object {
     //static properties
     protected static  $db_table ="comments";
     protected static $db_table_fields = [
+        'id',
         'photo_id',
         'author',
         'body',
     ];
 
     // properties
-    public $id;
-    public $photo_id;
-    public $author;
-    public $body;
+    public int $id;
+    public int $photo_id;
+    public string $author;
+    public string $body;
 
+ public  static   function  create_comment($photo_id,$author="John",$body="")
+ {
+     if (!empty($photo_id) && !empty($author) && !empty($body)){
 
+         $comment = new Comment();
+         $comment ->photo_id    = $photo_id;
+         $comment ->author      = $author;
+         $comment ->body        = $body;
+
+         return $comment;
+     }else{
+         return  false;
+     }
+ }
 
 }
