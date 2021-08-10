6ff7ac75b99cd395772115e318c5833fb0842a51
chisumo <bchisumo74@yahoo.com>
2021-08-10 22:52:21 +0100

    Implemented the set up of the previous  link

--- index.php
+++ index.php
@@ -39,9 +39,15 @@
                <div class="row">
                    <ul class="pager">
                        <?php
-                           if ($paginate->page_total() > 1){
-                               if ($paginate->has_next()){
-                                   echo "<li class='next'><a href='index.php?page={$paginate->next()}'>Next</a></li>";
+                           if ($paginate->page_total() > 1) {
+                               if ($paginate->has_next()) {
+                                   echo "<li class='next'>
+                                                    <a href='index.php?page={$paginate->next()}'>Next</a>
+                                         </li>";
+                               }
+
+                               if ($paginate->has_previous()) {
+                                   echo "<li class='previous'><a href='index.php?page={$paginate->previous()}'>Previous</a></li>";
                                }
                            }
                        ?>
