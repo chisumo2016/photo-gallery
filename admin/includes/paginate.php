b3624711d391f5a94dab707b2bc7e12768873078
chisumo <bchisumo74@yahoo.com>
2021-08-10 22:13:48 +0100

    implemented the paginate method in Paginate class

--- admin/includes/paginate.php
+++ admin/includes/paginate.php
@@ -14,4 +14,19 @@ class  Paginate{
         $this->items_total_count    = (int)$items_total_count;
     }
 
+    public  function next()
+    {
+        return $this->current_page + 1;
+    }
+
+    public  function previous()
+    {
+        return $this->current_page - 1;
+    }
+
+    public  function  page_total()
+    {
+        return ceil($this->items_total_count / $this->items_per_page);
+    }
+
 }
