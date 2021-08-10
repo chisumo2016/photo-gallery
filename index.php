6cbba56ac4637d6a9c0af913d866c3a615c3eb7e
chisumo <bchisumo74@yahoo.com>
2021-08-10 23:04:11 +0100

    Implemented the paginate Indicator and looping it to show the active and inactive class in index page

--- index.php
+++ index.php
@@ -36,15 +36,31 @@
                             </div>
                    <?php endforeach;?>
                </div>
+
                <div class="row">
                    <ul class="pager">
                        <?php
                            if ($paginate->page_total() > 1) {
                                if ($paginate->has_next()) {
                                    echo "<li class='next'>
-                                                    <a href='index.php?page={$paginate->next()}'>Next</a>
+                                             <a href='index.php?page={$paginate->next()}'>
+                                                Next
+                                             </a>
                                          </li>";
                                }
+
+                               for ($i = 1; $i <= $paginate->page_total(); $i++)
+                               {
+                                   if ($i == $paginate->current_page){
+
+                                       echo "<li class='active'><a href='index.php?page={$i}'>{$i}</a></li>";
+
+                                   }else{
+                                       echo "<li><a href='index.php?page={$i}'>{$i}</a></li>";
+                                   }
+                               }
+
+
 
                                if ($paginate->has_previous()) {
                                    echo "<li class='previous'><a href='index.php?page={$paginate->previous()}'>Previous</a></li>";
