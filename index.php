77a1e8172bd46f3ec73e2b412a5ff79d7f6714b3
chisumo <bchisumo74@yahoo.com>
2021-08-10 19:55:07 +0100

    Setting Up Index to Display Photos

--- index.php
+++ index.php
@@ -1,30 +1,38 @@
 <?php include("includes/header.php"); ?>
+<?php
 
+     $photos = Photo :: find_all();
+?>
 
         <div class="row">
 
             <!-- Blog Entries Column -->
-            <div class="col-md-8">
+           <div class="col-md-12">
+               <div class="thumbnails row">
+                   <?php  foreach ($photos as $photo) : ?>
 
-    
-            
-          
-         
+                            <div class="col-xs-6 col-md-3">
+                                <a href="" class="thumbnail">
+                                    <img src="admin/<?php echo $photo->picture_path();?>" alt="">
 
-            </div>
+                                </a>
+                            </div>
+                   <?php endforeach;?>
+               </div>
+           </div>
 
 
 
 
             <!-- Blog Sidebar Widgets Column -->
-            <div class="col-md-4">
+<!--            <div class="col-md-4">-->
+<!---->
+<!--            -->
+<!--                 --><?php //include("includes/sidebar.php"); ?>
+<!---->
+<!---->
 
-            
-                 <?php include("includes/sidebar.php"); ?>
-
-
-
-        </div>
+ </div>
         <!-- /.row -->
 
         <?php include("includes/footer.php"); ?>
